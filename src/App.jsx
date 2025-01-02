cf5678fe5bb26aee03794cc58fe3da26f8fb6cf0
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-01-02 09:29:04 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="grid grid-cols-10">
-        <div className="bg-green-50 col-span-4">ewjhi</div>
-        <div className="bg-white col-span-4">asdljhf</div>
-        <div className="bg-black col-span-2">fdsaio</div>
+      <div className=" grid grid-cols-12">
+        <div className="bg-green-50 col-span-12 md:col-span-4">ewjhi</div>
+        <div className="bg-white  col-span-12 md:col-span-4">asdljhf</div>
+        <div className="bg-black col-span-12 md:col-span-4 ">fdsaio</div>
       </div>
     </>
   );
