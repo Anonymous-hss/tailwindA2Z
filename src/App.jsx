aeb4e922219459e262f59affd08284ed449edf57
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-01-02 09:12:03 +0530

    grids

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-black">Hello </div>
+      <div className="grid grid-cols-10">
+        <div className="bg-green-50 col-span-4">ewjhi</div>
+        <div className="bg-white col-span-4">asdljhf</div>
+        <div className="bg-black col-span-2">fdsaio</div>
+      </div>
     </>
   );
 }
